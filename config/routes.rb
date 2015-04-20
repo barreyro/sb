c5b5e4b9e244828f00c82b7ab8e5a8456d70ccbf
Stephanie <stephaniebarreyro@gmail.com>
2015-04-20 01:23:38 -0700

    Create routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,11 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
 
+  resources :projects
+
+  resources :contacts do
+    get '/contacts' => 'contacts#new'
+    post 'contacts' => 'contacts#create'
+  end
+end
