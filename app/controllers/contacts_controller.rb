7de5325b30df0de70390a3ab2e949a89cc50cf2e
Stephanie <stephaniebarreyro@gmail.com>
2015-04-20 07:50:25 -0700

    Contact form method created.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,31 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      session[:id] = @contact.id
+      redirect_to :root
+    else
+      redirect_to projects_path
+    end
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comment)
   end
 end
