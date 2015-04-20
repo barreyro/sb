931bb4afff194b8138a5eb8f87cce575146e1be4
Stephanie <stephaniebarreyro@gmail.com>
2015-04-20 01:54:33 -0700

    Add Contacts model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   root 'welcome#index'
 
-  resources :projects
-
-  resources :contacts do
-    get '/contacts' => 'contacts#new'
-    post 'contacts' => 'contacts#create'
-  end
+  resources :projects, :contacts
 end
